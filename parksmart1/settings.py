7262335e027eafb279b308939802247d64a90529
varsha <varshabaddi12@gmail.com>
2026-04-09 22:06:21 +0530

    actual fix for static root

--- parksmart1/settings.py
+++ parksmart1/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles" #Updated for Render
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
