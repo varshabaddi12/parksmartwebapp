696b864f2e1d5b048c68111ae78cc785651982bf
varsha <varshabaddi12@gmail.com>
2026-04-09 23:46:49 +0530

    add auto-admin creator

--- parksmart1/wsgi.py
+++ parksmart1/wsgi.py
@@ -14,3 +14,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parksmart1.settings')
 
 application = get_wsgi_application()
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    print("Superuser created successfully!")
